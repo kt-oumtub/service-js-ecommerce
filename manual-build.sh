956c84412d4eb141f7fc58d79459af43e6b01e04
Kitti <kt.oumtub@gmail.com>
2024-10-20 16:24:54 +0700

    update docker compose

--- manual-build.sh
+++ manual-build.sh
@@ -1,6 +1,9 @@
 
-docker build -t service-js-ecommerce .
+# docker build -t service-js-ecommerce .
+docker build -t kittioumtub/service-js-ecommerce .
 
-docker push 123454321.dkr.ecr.ap-southeast-1.amazonaws.com/my-admin-api-cur-curriculum:$1
+docker login
+
+docker push kittioumtub/service-js-ecommerce:latest
 
 echo "Success!"
